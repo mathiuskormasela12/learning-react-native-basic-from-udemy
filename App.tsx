25448c6aec8ee0b7f27ce260fdcf36c1935c1fc9
Mathius <mathiuskormasela12@gmail.com>
2023-10-20 23:17:56 +0700

    basic style

--- App.tsx
+++ App.tsx
@@ -1,23 +1,49 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
-import {StatusBar} from "expo-status-bar"
+import { StyleSheet, View, Text, Button } from "react-native";
 
 const App: React.FC = () => {
   return (
+    // View component is used to hold another component, like div in html but View component can't wrap text
     <View style={styles.container}>
-      <Text>Hello World!!!</Text>
-      <StatusBar style="auto" />
+     <View>
+      {
+        // Text component is used to wrap text
+      }
+      <Text style={styles.text}>Another piece of text!</Text>
+      {
+        // Define inline styles
+      }
+      <Text style={{color: 'red',  
+        padding: 12,
+        margin: 12,
+        borderWidth: 2,
+        borderColor: 'blue'
+        }}
+      >Another piece of text!</Text>
+     </View>
+     <Text style={styles.text}>Hello Wordl!</Text>
+     <Button title="Tap Me" />
     </View>
   )
 }
 
 export default App;
 
+// Define styles
+// if we define style using StyleSheet, we will be having auto-complete and will improve performance.
+// if we fill style property with wrong value, React Native will show error. But if we use plain object instead, 
+// React Native will not throw error. So we recommend us to use StyleSheet.create to define style
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  text: {
+    padding: 12,
+    margin: 12,
+    borderWidth: 2,
+    borderColor: 'blue'
   }
 })
