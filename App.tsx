58f007cc167867e292d39a7d55dec33ae81581da
Mathius <mathiuskormasela12@gmail.com>
2023-10-21 21:02:41 +0700

    Android & iOS Style Differences and ScrollView

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, Button, TextInput, StyleSheet } from "react-native";
+import { View, Text, Button, TextInput, ScrollView, StyleSheet } from "react-native";
 
 const App: React.FC = () => {
   const [enteredGoalText, setEnteredGoalText] = useState<string>('');
@@ -19,9 +19,36 @@ const App: React.FC = () => {
         <Button title="Add Goal" onPress={handleAddGoal} />
       </View>
       <View style={styles.goalContainer}>
-        {courseGoal.map((goal) => (
-          <Text key={goal.toString()}>{goal}</Text>
-        ))}
+        {/* 
+          ScrollView Component is used to make 
+          the child component scrollable. It can be used
+          if we want to create scrollable layout, if we want to make
+          sure our layout can work nicely on each devices when we don't
+          know which device that user will use (maybe user will use our app
+          on small device and it will make our content is hidden when the 
+          content size is overflow, so in order to fix it, we can use
+          ScrollView component).
+
+          But ScrollView will render all items 
+          at once when component is rendered. So for example
+          if we have 5000 items, then those items will be rendered
+          at once and it will slow down our app.
+
+          So don't we use ScrollView on that case, we can use
+          FlatList component instead.
+
+          So actually we will use ScrollView when we want to
+          create scrollable layout. but when we want to create
+          dynamic list, we can use FlatList instead.
+        */}
+        <ScrollView>
+          {courseGoal.map((goal) => (
+            // <Text key={goal.toString()} style={styles.goalItem}>{goal}</Text>
+            <View key={goal.toString()} style={styles.goalItem}>
+              <Text style={styles.goalText}>{goal}</Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </View>
   );
@@ -53,5 +80,24 @@ const styles = StyleSheet.create({
   },
   goalContainer: {
     flex: 3
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    backgroundColor: '#560acc',
+    // color: 'white',
+    /* This won't work on iOS, because in iOS 
+    Text component in iOS don't have border 
+    radius, but on Android has it, 
+    so it works on Android only.
+    In order to fix that issue, we can wrap 
+    the Text component to View Component, because
+    View component is a general component that have border
+    radius. So it means, sometimes we have to write different style
+    in order to make our styles works on both platform. */
+    borderRadius: 6
+  },
+  goalText: {
+    color: 'white'
   }
 });
